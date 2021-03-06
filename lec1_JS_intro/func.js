de05ca1ae9b5dd092c5de07c00f06266226605fa
NiteshKumar <nk6511165@gmail.com>
2021-03-06 20:00:08 +0530

    js class

---
+++ lec1_JS_intro/func.js
@@ -0,0 +1,10 @@
+// function harry(){
+//     console.log("You are in harry poter world");
+// }
+// harry();
+
+function nangloi(name,id){
+    console.log(name+" "+id);
+}
+nangloi("nitesh",1);
+
---
+++ lec1_JS_intro/intro.js
@@ -0,0 +1,26 @@
+let a=10;
+let b="nik";
+let c=2.13;
+let d=true;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+let bio={
+    "name":"Nitesh",
+    "lastname":"Kumar",
+    "email":"nk6511165@gmail.com"
+}
+// console.log(bio.email,bio.name);
+let arr=["king",
+          "OP",
+          "Thor",
+          bio,
+          [2,3,4,5]
+         ];
+// console.log(arr); 
+const pi=3.14;
+// console.log(pi);
+Number kaluwa=3;
+console.log(kaluwa);
+
